7e6f5cd5b5c6a15c901bf609e3d87dd82bf97805
EC2 Default User <ec2-user@ip-172-31-21-102.ec2.internal>
2023-08-14 02:38:57 +0000

    minor fixes to yaml files

--- .c9/metadata/environment/terraform/output.tf
+++ .c9/metadata/environment/terraform/output.tf
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 {"filter":false,"title":"output.tf","tooltip":"/terraform/output.tf","undoManager":{"mark":-1,"position":-1,"stack":[]},"ace":{"folds":[],"scrolltop":0,"scrollleft":0,"selection":{"start":{"row":0,"column":0},"end":{"row":0,"column":0},"isBackwards":false},"options":{"guessTabSize":true,"useWrapMode":false,"wrapToView":true},"firstLineState":0},"timestamp":1691934050688,"hash":"dbf3280ba590115b53fafb7ddde4daaeb3083d47"}
+=======
+{"filter":false,"title":"output.tf","tooltip":"/terraform/output.tf","ace":{"folds":[],"scrolltop":0,"scrollleft":0,"selection":{"start":{"row":0,"column":0},"end":{"row":0,"column":0},"isBackwards":false},"options":{"guessTabSize":true,"useWrapMode":false,"wrapToView":true},"firstLineState":0},"hash":"dbf3280ba590115b53fafb7ddde4daaeb3083d47","undoManager":{"mark":-1,"position":-1,"stack":[]},"timestamp":1691901282971}
+>>>>>>> 39ca512 (minor fixes to the yaml files)
--- .c9/metadata/environment/terraform/sg.tf
+++ .c9/metadata/environment/terraform/sg.tf
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 {"filter":false,"title":"sg.tf","tooltip":"/terraform/sg.tf","undoManager":{"mark":-1,"position":-1,"stack":[]},"ace":{"folds":[],"scrolltop":194,"scrollleft":0,"selection":{"start":{"row":0,"column":0},"end":{"row":0,"column":0},"isBackwards":false},"options":{"guessTabSize":true,"useWrapMode":false,"wrapToView":true},"firstLineState":0},"timestamp":1691934052122,"hash":"e696cc08b13d305f7480523d06f7dd3dfa688be5"}
+=======
+{"filter":false,"title":"sg.tf","tooltip":"/terraform/sg.tf","ace":{"folds":[],"scrolltop":194,"scrollleft":0,"selection":{"start":{"row":0,"column":0},"end":{"row":0,"column":0},"isBackwards":false},"options":{"guessTabSize":true,"useWrapMode":false,"wrapToView":true},"firstLineState":0},"hash":"e696cc08b13d305f7480523d06f7dd3dfa688be5","undoManager":{"mark":-1,"position":-1,"stack":[]},"timestamp":1691901282967}
+>>>>>>> 39ca512 (minor fixes to the yaml files)
